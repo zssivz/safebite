4ab2dbe225cc131b7d18d79006dbe87b8543e41d
Assiva Nurul <80864647+zssivz@users.noreply.github.com>
2023-05-31 06:28:07 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://your-project-id.firebaseio.com' // Ganti dengan URL database Firebase Anda
+  databaseURL: 'https://your-project-id.firebaseio.com' // Ganti dengan URL database Firebase
 });
 
 const db = admin.firestore();
