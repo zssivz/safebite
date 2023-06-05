ea51b0130a921d644ec5db59ad26b79f85289369
Assiva Nurul <80864647+zssivz@users.noreply.github.com>
2023-06-05 10:09:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,109 @@
 const express = require('express');
-const { db } = require('./firebase');
-
+const bodyParser = require('body-parser');
 const app = express();
-app.use(express.json());
+const admin = require('firebase-admin');
 
-// Endpoint untuk registrasi pengguna
-app.post('/register', async (req, res) => {
-  try {
-    const { nama, number_of_allergen, list_of_allergen, halal } = req.body;
-
-    // Lakukan validasi data pengguna
-    if (!nama || !number_of_allergen || !list_of_allergen || !halal) {
-      return res.status(400).json({ error: 'Data pengguna tidak lengkap' });
-    }
-
-    // Simpan data pengguna ke Firestore
-    const userRef = db.collection('users').doc();
-    await userRef.set({
-      nama,
-      umur,
-      number_of_allergen,
-      list_of_allergen,
-      halal
-    });
-
-    res.status(201).json({ message: 'Registrasi berhasil' });
-  } catch (error) {
-    console.error('Error saat registrasi pengguna:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat melakukan registrasi' });
-  }
+// Inisialisasi konfigurasi proyek Firebase
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://safebitecapstone-d3dc2.firebaseio.com' 
 });
 
-// Endpoint untuk fitur deteksi gambar
-app.post('/detect', async (req, res) => {
-  try {
-    const { image } = req.body;
+// Dapatkan referensi koleksi Firestore yang ingin digunakan
+const firestore = admin.firestore();
+const datasetRef = firestore.collection('datasets');
 
-    // Lakukan deteksi gambar menggunakan model machine learning
+// Tambahkan middleware body-parser untuk membaca data JSON dari request body
+app.use(bodyParser.json());
 
-    // Ambil data produk dari Firestore berdasarkan hasil deteksi
+// PREFERENSI ALLERGEN
+// Deklarasikan variabel global untuk menyimpan preferensi allergen
+let preferensiAllergen = [];
 
-    res.status(200).json({ productData: /* data produk dari Firestore */ });
-  } catch (error) {
-    console.error('Error saat mendeteksi gambar:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat mendeteksi gambar' });
-  }
+// Endpoint untuk mengambil data preferensi allergen
+app.get('/api/preferensi/allergen', (req, res) => {
+  // Mengembalikan preferensi allergen yang telah disimpan
+  res.json(preferensiAllergen);
 });
 
-// Endpoint untuk fitur histori
-app.get('/history', async (req, res) => {
-  try {
-    // Ambil histori pengguna dari Firestore
+// Endpoint untuk menyimpan data preferensi allergen
+app.post('/api/preferensi/allergen', (req, res) => {
+  // Mendapatkan data preferensi allergen dari request body
+  const dataPreferensiAllergen = req.body.preferensiAllergen;
 
-    res.status(200).json({ history: /* data histori dari Firestore */ });
-  } catch (error) {
-    console.error('Error saat mengambil histori:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil histori' });
-  }
+  // Menyimpan data preferensi allergen ke variabel global
+  preferensiAllergen = dataPreferensiAllergen;
+
+  // Mengirim response berhasil
+  res.send('Preferensi allergen berhasil disimpan');
 });
 
-// Jalankan server
+// PREFERENSI HALAL/NETRAL
+// Deklarasikan variabel global untuk menyimpan preferensi halal
+let preferensiHalal = '';
+
+// Mengambil data preferensi halal yang dipilih oleh user
+app.get('/api/preferensi/halal', (req, res) => {
+  // Mengembalikan preferensi halal yang telah disimpan
+  res.json(preferensiHalal);
+});
+
+// Menyimpan data preferensi halal yang dipilih oleh user
+app.post('/api/preferensi/halal', (req, res) => {
+  // Mendapatkan data preferensi halal dari request body
+  const dataPreferensiHalal = req.body.preferensiHalal;
+
+  // Menyimpan data preferensi halal ke variabel global
+  preferensiHalal = dataPreferensiHalal;
+
+  // Mengirim response berhasil
+  res.send('Preferensi halal berhasil disimpan');
+});
+
+// AMBIL TEXT HASIL OCR
+// Endpoint untuk mengambil teks dari hasil OCR ML Kit
+app.post('/api/ocr', (req, res) => {
+  // Implementasikan logika untuk mengambil teks dari hasil OCR ML Kit
+  // yang dikirim dari Android Studio dan mengembalikan response
+  const ocrText = req.body.text;
+  res.json(ocrText);
+});
+
+// Endpoint untuk melakukan proses deteksi dengan model machine learning
+app.post('/api/deteksi', (req, res) => {
+  // Implementasikan logika untuk proses deteksi menggunakan model machine learning
+  // dengan dataset dari Firestore yang dikirim dari client dan mengembalikan response
+  const dataset = req.body.dataset;
+  // Proses deteksi menggunakan dataset
+  const hasilDeteksi = 'Hasil deteksi';
+  res.json(hasilDeteksi);
+});
+
+// HASIL DETEKSI
+// Deklarasikan variabel global untuk menyimpan hasil deteksi
+let hasilDeteksi = '';
+
+// Mengambil hasil proses deteksi
+app.get('/api/hasil', (req, res) => {
+  // Mengembalikan hasil deteksi yang telah disimpan
+  res.json(hasilDeteksi);
+});
+
+// Menyimpan hasil proses deteksi
+app.post('/api/hasil', (req, res) => {
+  // Mendapatkan hasil deteksi dari request body
+  const dataHasilDeteksi = req.body.hasilDeteksi;
+
+  // Menyimpan hasil deteksi ke variabel global
+  hasilDeteksi = dataHasilDeteksi;
+
+  // Mengirim response berhasil
+  res.send('Hasil deteksi berhasil disimpan');
+});
+
+
+// Menjalankan server pada port
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
